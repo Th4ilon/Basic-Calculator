aaa235a4f656dcb1517641647c95a7aaec1f8c53
venan85 <venan_476@hotmail.com>
2018-05-08 20:40:03 +0200

    arreglado un error en la ruta a la carpeta js, tenia un punto y eran necesarios 2

---
+++ view/main.js
@@ -0,0 +1,157 @@
+//$(document).ready(function(){
+
+    //var valor;
+	//var resultadoTotal;
+    //var valor2;
+    //var operacionFinal = "";
+
+var operandoa;
+var operandob;
+var operacion;
+function init(){
+    
+    var resultado = document.getElementById('resultado');
+    var reset = document.getElementById('reset');
+    var suma = document.getElementById('suma');
+    var resta = document.getElementById('resta');
+    var multiplicacion = document.getElementById('multiplicacion');
+    var division = document.getElementById('division');
+    var igual = document.getElementById('igual');
+    var uno = document.getElementById('uno');
+    var dos = document.getElementById('dos');
+    var tres = document.getElementById('tres');
+    var cuatro = document.getElementById('cuatro');
+    var cinco = document.getElementById('cinco');
+    var seis = document.getElementById('seis');
+    var siete = document.getElementById('siete');
+    var ocho = document.getElementById('ocho');
+    var nueve = document.getElementById('nueve');
+    var cero = document.getElementById('cero');
+  
+    uno.onclick = function(e){
+        resultado.textContent = resultado.textContent  + "1";
+    }
+    dos.onclick = function(e){
+        resultado.textContent = resultado.textContent  + "2";
+    }
+    tres.onclick = function(e){
+        resultado.textContent = resultado.textContent  + "3";
+    }
+    cuatro.onclick = function(e){
+        resultado.textContent = resultado.textContent  + "4";
+    }
+    cinco.onclick = function(e){
+        resultado.textContent = resultado.textContent  + "5";
+    }
+    seis.onclick = function(e){
+        resultado.textContent = resultado.textContent  + "6";
+    }
+    siete.onclick = function(e){
+        resultado.textContent = resultado.textContent  + "7";
+    }
+    ocho.onclick = function(e){
+        resultado.textContent = resultado.textContent  + "8";
+    }
+    nueve.onclick = function(e){
+        resultado.textContent = resultado.textContent  + "9";
+    }
+    cero.onclick = function(e){
+        resultado.textContent = resultado.textContent  + "0";
+    }
+    reset.onclick = function(e){
+        resetear();
+    }
+    suma.onclick = function(e){
+        operandoa = resultado.textContent;
+        operacion = "+";
+        limpiar();
+    }
+    resta.onclick = function(e){
+        operandoa = resultado.textContent;
+        operacion = "-";
+        limpiar();
+    }
+    multiplicacion.onclick = function(e){
+        operandoa = resultado.textContent;
+        operacion = "*";
+        limpiar();
+    }
+    division.onclick = function(e){
+        operandoa = resultado.textContent;
+        operacion = "/";
+        limpiar();
+    }
+    igual.onclick = function(e){
+        operandob = resultado.textContent;
+        resolver();
+    }
+}
+function limpiar(){
+    resultado.textContent = "";
+  }
+  function resetear(){
+    resultado.textContent = "";
+    operandoa = 0;
+    operandob = 0;
+    operacion = "";
+  }
+  function resolver(){
+    var res = 0;
+    switch(operacion){
+      case "+":
+        res = parseFloat(operandoa) + parseFloat(operandob);
+        break;
+      case "-":
+          res = parseFloat(operandoa) - parseFloat(operandob);
+          break;
+      case "*":
+        res = parseFloat(operandoa) * parseFloat(operandob);
+        break;
+      case "/":
+        res = parseFloat(operandoa) / parseFloat(operandob);
+        break;
+    }
+    resetear();
+    resultado.textContent = res;
+  }
+    /**
+     * Operacion.
+     */
+	//Operacion = function Operacion(operacion)
+	//{
+       // valor = document.getElementById("numero").value;    
+        //operacionFinal = operacion;
+	//}
+	/**
+     * Funcion resultado .
+     */	
+    //resultado = function resultado()
+	//{
+        //valor2 = document.getElementById("numero").value;
+        //var numero1=parseInt(valor);
+        //var numero2=parseInt(valor2);
+        //if (operacionFinal==="+") 
+        //{
+          // operacionFinal="";
+		   //resultadoTotal = numero1+numero2;
+        //} 
+        //else if(operacionFinal==="*") 
+        //{
+          //  operacionFinal="";
+            //resultadoTotal = numero1*numero2;	
+        //}
+        //else if(operacionFinal==="/")
+        //{
+          //  operacionFinal="";
+            //resultadoTotal = numero1/numero2;
+        //}
+        //else if(operacionFinal==="-")
+        //{
+          //  operacionFinal="";
+            //resultadoTotal = numero1-numero2;
+        //}
+	    //console.log (resultadoTotal);
+    //}
+   
+//});
+   
