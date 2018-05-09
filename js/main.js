86c9c67f2fb42bff09c881365a7a5dfe1c50479c
venan85 <venan_476@hotmail.com>
2018-05-09 21:59:49 +0200

    cambiamos todas las funciones de la calculadora a jquery y conseguimos que sea completamente funcional

--- js/main.js
+++ js/main.js
@@ -9,83 +9,60 @@ var operandoa;
 var operandob;
 var operacion;
 function init(){
-    
     var resultado = document.getElementById('resultado');
-    var reset = document.getElementById('reset');
-    var suma = document.getElementById('suma');
-    var resta = document.getElementById('resta');
-    var multiplicacion = document.getElementById('multiplicacion');
-    var division = document.getElementById('division');
-    var igual = document.getElementById('igual');
-    var uno = document.getElementById('uno');
-    var dos = document.getElementById('dos');
-    var tres = document.getElementById('tres');
-    var cuatro = document.getElementById('cuatro');
-    var cinco = document.getElementById('cinco');
-    var seis = document.getElementById('seis');
-    var siete = document.getElementById('siete');
-    var ocho = document.getElementById('ocho');
-    var nueve = document.getElementById('nueve');
-    var cero = document.getElementById('cero');
+    /*$(".test").on("click",function (e) {
+        console.log($(this).val());
+        resultado.textContent = resultado.textContent  + $(this).val();
+    });*/
+    /**
+     * 
+     * 
+     */
+  $(".numericButtons").on("click",function (e) {
+    var valorDeEvento = $(this).val();
+    resultado.textContent = resultado.textContent + $(this).val();
+  });
+  $(".operadores").on("click",function (e) {
+      var valorDeEvento = $(this).val();
+      switch (valorDeEvento) {
+          case "suma":
+            operandoa = resultado.textContent;
+            operacion=valorDeEvento;
+            limpiar();
+            break;
+          case "division":
+            operandoa = resultado.textContent;
+            operacion=valorDeEvento;
+            limpiar();
+            break;
+          case "multiplicacion":
+            operandoa = resultado.textContent;
+            operacion=valorDeEvento;
+            limpiar();
+            break;
+          case "resta":
+            operandoa = resultado.textContent;
+            operacion=valorDeEvento;
+            limpiar();
+            break;
+
+        default:
+            break;
+      }
+  })
+   
   
-    uno.onclick = function(e){
-        resultado.textContent = resultado.textContent  + "1";
-    }
-    dos.onclick = function(e){
-        resultado.textContent = resultado.textContent  + "2";
-    }
-    tres.onclick = function(e){
-        resultado.textContent = resultado.textContent  + "3";
-    }
-    cuatro.onclick = function(e){
-        resultado.textContent = resultado.textContent  + "4";
-    }
-    cinco.onclick = function(e){
-        resultado.textContent = resultado.textContent  + "5";
-    }
-    seis.onclick = function(e){
-        resultado.textContent = resultado.textContent  + "6";
-    }
-    siete.onclick = function(e){
-        resultado.textContent = resultado.textContent  + "7";
-    }
-    ocho.onclick = function(e){
-        resultado.textContent = resultado.textContent  + "8";
-    }
-    nueve.onclick = function(e){
-        resultado.textContent = resultado.textContent  + "9";
-    }
-    cero.onclick = function(e){
-        resultado.textContent = resultado.textContent  + "0";
-    }
+   
     reset.onclick = function(e){
         resetear();
     }
-    suma.onclick = function(e){
-        operandoa = resultado.textContent;
-        operacion = "+";
-        limpiar();
-    }
-    resta.onclick = function(e){
-        operandoa = resultado.textContent;
-        operacion = "-";
-        limpiar();
-    }
-    multiplicacion.onclick = function(e){
-        operandoa = resultado.textContent;
-        operacion = "*";
-        limpiar();
-    }
-    division.onclick = function(e){
-        operandoa = resultado.textContent;
-        operacion = "/";
-        limpiar();
+   
     }
     igual.onclick = function(e){
         operandob = resultado.textContent;
         resolver();
     }
-}
+
 function limpiar(){
     resultado.textContent = "";
   }
@@ -95,25 +72,28 @@ function limpiar(){
     operandob = 0;
     operacion = "";
   }
-  function resolver(){
+  $("#igual").on("click", function resolver(){
     var res = 0;
     switch(operacion){
-      case "+":
+      case "suma":
         res = parseFloat(operandoa) + parseFloat(operandob);
         break;
-      case "-":
+      case "resta":
           res = parseFloat(operandoa) - parseFloat(operandob);
           break;
-      case "*":
+      case "multiplicacion":
         res = parseFloat(operandoa) * parseFloat(operandob);
         break;
-      case "/":
+      case "division":
         res = parseFloat(operandoa) / parseFloat(operandob);
         break;
+      default:
+        limpiar();
+         break;
     }
     resetear();
     resultado.textContent = res;
-  }
+  });
     /**
      * Operacion.
      */
